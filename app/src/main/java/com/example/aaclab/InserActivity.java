2085035109006197b6fd0947e253a46f9398fe32
allenwuadx <allen.wu.adx@gmail.com>
2019-06-27 10:31:58 +0800

    Add beverage feature complete

---
+++ app/src/main/java/com/example/aaclab/InserActivity.java
@@ -0,0 +1,41 @@
+package com.example.aaclab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class InserActivity extends AppCompatActivity {
+    public static final String ADD_TITLE = "beverage.add.title";
+    public static final String ADD_DETAIL = "beverage.add.detail";
+
+    @BindView(R.id.submitButton)
+    Button button;
+    @BindView(R.id.titleEditText)
+    EditText titleEditText;
+    @BindView(R.id.detailEditText)
+    EditText detailEditText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        ButterKnife.bind(this);
+        button.setOnClickListener(v->addButtonCallback());
+    }
+
+    private void addButtonCallback() {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        bundle.putString(ADD_TITLE, titleEditText.getText().toString());
+        bundle.putString(ADD_DETAIL, detailEditText.getText().toString());
+        intent.putExtras(bundle);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/aaclab/MainActivity.java
+++ app/src/main/java/com/example/aaclab/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.aaclab;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static com.example.aaclab.InserActivity.ADD_DETAIL;
+import static com.example.aaclab.InserActivity.ADD_TITLE;
 
 public class MainActivity extends AppCompatActivity {
     private BeverageViewModel mViewModel;
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
+    @BindView(R.id.addButton)
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initBeverageAdapter();
+        addButton.setOnClickListener(v->addButtonCallback());
+    }
+
+    private static final int ADD_ACTIVITY = 1001;
+
+    private void addButtonCallback() {
+        Intent intent = new Intent(this,InserActivity.class);
+        startActivityForResult(intent,ADD_ACTIVITY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
+        switch (requestCode){
+            case ADD_ACTIVITY:
+                if(resultCode==RESULT_OK) {
+                    Bundle bundle = data.getExtras();
+                    String title = bundle.getString(ADD_TITLE);
+                    String detail = bundle.getString(ADD_DETAIL);
+                    BeverageEntity entity = new BeverageEntity(title, detail);
+                    mViewModel.insert(entity);
+                } else {
+                    Toast.makeText(this, "not saved", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 
     private void initBeverageAdapter() {
